d1932cc3ad0b05c90622c5e1c569f2f066fd3fb3
Kayla <golderkayla@gmail.com>
2020-06-06 13:20:29 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'client/src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test:/\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
